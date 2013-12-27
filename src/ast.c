b765a8e1f37cfe9f40a80f348ce69a60be2fc952
Kate F <kate@elide.org>
2013-12-27 22:27:36 +0000

    An AST.

---
+++ src/ast.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ast.h"
+
+struct ast_arg *
+ast_new_arg(size_t n, const char *s)
+{
+	struct ast_arg *arg;
+
+	assert(s != NULL);
+
+	arg = malloc(sizeof *arg + n + 1);
+	if (arg == NULL) {
+		return NULL;
+	}
+
+	arg->s    = (char *) arg + sizeof *arg;
+	arg->next = NULL;
+
+	memcpy(arg->s, s, n);
+	arg->s[n] = '\0';
+
+	return arg;
+}
+
+struct ast_exec *
+ast_new_exec(size_t n, const char *s)
+{
+	struct ast_exec *exec;
+
+	assert(s != NULL);
+
+	exec = malloc(sizeof *exec + n + 1);
+	if (exec == NULL) {
+		return NULL;
+	}
+
+	exec->s   = (char *) exec + sizeof *exec;
+	exec->arg = NULL;
+
+	memcpy(exec->s, s, n);
+	exec->s[n] = '\0';
+
+	return exec;
+}
+
+struct ast_node *
+ast_new_node_exec(struct ast_exec *exec)
+{
+	struct ast_node *node;
+
+	assert(exec != NULL);
+
+	node = malloc(sizeof *node);
+	if (node == NULL) {
+		return NULL;
+	}
+
+	node->type   = AST_NODE_EXE;
+	node->u.exec = exec;
+	node->next   = NULL;
+
+	return node;
+}
+
+struct ast_node *
+ast_new_node_node(struct ast_node *child)
+{
+	struct ast_node *node;
+
+	assert(child != NULL);
+
+	node = malloc(sizeof *node);
+	if (node == NULL) {
+		return NULL;
+	}
+
+	node->type   = AST_NODE_CMD;
+	node->u.node = child;
+	node->next   = NULL;
+
+	return node;
+}
+
---
+++ src/ast.h
@@ -0,0 +1,48 @@
+#ifndef AST_H
+#define AST_H
+
+enum ast_node_type {
+	AST_NODE_EXE,
+	AST_NODE_CMD
+};
+
+struct ast_arg {
+	char *s;
+	struct ast_arg *next;
+};
+
+struct ast_exec {
+	char *s;
+	struct ast_arg *arg;
+};
+
+struct ast_node {
+	enum ast_node_type type;
+	union {
+		struct ast_exec *exec;
+		struct ast_node *node; /* conceptually equivalent to a block */
+		/* TODO: other command types: functions, conditionals, assignments etc here */
+	} u;
+
+	/* TODO: locally-scoped variables here */
+
+	struct ast_node *next;
+};
+
+struct ast_arg *
+ast_new_arg(size_t n, const char *s);
+
+struct ast_exec *
+ast_new_exec(size_t n, const char *s);
+
+struct ast_node *
+ast_new_node_exec(struct ast_exec *exec);
+
+struct ast_node *
+ast_new_node_node(struct ast_node *node);
+
+int
+ast_dump(const struct ast_node *node);
+
+#endif
+
