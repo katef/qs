671eb5d186638b0e1a1a175fb4a5b7d143941592
Kate F <kate@elide.org>
2014-01-12 13:30:14 +0000

    Replaced the AST with a BET.

---
+++ src/bet.c
@@ -0,0 +1,119 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "bet.h"
+
+struct bet *
+bet_new_leaf(enum bet_type type, const char *s)
+{
+	struct bet *new;
+
+	assert(s = NULL);
+
+	new = malloc(sizeof *new + strlen(s) + 1);
+	if (new == NULL) {
+		return NULL;
+	}
+
+	new->type = type;
+	new->u.s  = strcpy((char *) new + sizeof *new, s);
+
+	return new;
+}
+
+struct bet *
+bet_new_branch(enum bet_type type, struct bet *a, struct bet *b)
+{
+	struct bet *new;
+
+	new = malloc(sizeof *new);
+	if (new == NULL) {
+		return NULL;
+	}
+
+	new->type   = type;
+	new->u.op.a = a;
+	new->u.op.b = b;
+
+	return new;
+}
+
+void
+bet_free(struct bet *bet)
+{
+	if (bet == NULL) {
+		return;
+	}
+
+	switch (bet->type) {
+	case BET_STR:
+	case BET_VAR:
+		break;
+
+	default:
+		bet_free(bet->u.op.a);
+		bet_free(bet->u.op.b);
+		break;
+	}
+
+	free(bet);
+}
+
+static int
+indent(unsigned n)
+{
+	while (n--) {
+		fprintf(stderr, "\t");
+	}
+
+	return 0;
+}
+
+static int
+dump_node(const struct bet *bet, unsigned n)
+{
+	const char *op;
+
+	if (-1 == indent(n)) {
+		return -1;
+	}
+
+	if (bet == NULL) {
+		fprintf(stderr, "NULL\n");
+		return 0;
+	}
+
+	switch (bet->type) {
+	case BET_STR: fprintf(stderr, "'%s'\n", bet->u.s); return 0;
+	case BET_VAR: fprintf(stderr, "$%s\n",  bet->u.s); return 0;
+
+	case BET_AND:    op = "&&"; break;
+	case BET_OR:     op = "||"; break;
+	case BET_JOIN:   op = "^";  break;
+	case BET_PIPE:   op = "|";  break;
+	case BET_ASSIGN: op = "=";  break;
+	case BET_EXEC:   op = ";";  break;
+	case BET_BG:     op = "&";  break;
+	case BET_LIST:   op = "->"; break;
+
+	default:
+		op = "?";
+		break;
+	}
+
+	fprintf(stderr, "%s\n", op);
+
+	dump_node(bet->u.op.a, n + 1);
+	dump_node(bet->u.op.b, n + 1);
+
+	return 0;
+}
+
+int
+bet_dump(const struct bet *bet)
+{
+	return dump_node(bet, 0);
+}
+
---
+++ src/bet.h
@@ -0,0 +1,46 @@
+#ifndef BET_H
+#define BET_H
+
+enum bet_type {
+	/* leaves */
+	BET_STR,    /* 'xyz' */
+	BET_VAR,    /* $xyz  */
+
+	/* branches */
+	BET_AND,    /* a && b */
+	BET_OR,     /* a || b */
+	BET_JOIN,   /* a  ^ b */
+	BET_PIPE,   /* a  | b */
+	BET_ASSIGN, /* a  = b */
+	BET_EXEC,   /* a  ; b */
+	BET_BG,     /* a  & b */
+	BET_LIST    /* a    b */
+};
+
+struct bet {
+	enum bet_type type;
+
+	union {
+		char *s;
+
+		struct {
+			struct bet *a;
+			struct bet *b;
+		} op;
+	} u;
+};
+
+struct bet *
+bet_new_leaf(enum bet_type type, const char *s);
+
+struct bet *
+bet_new_branch(enum bet_type type, struct bet *a, struct bet *b);
+
+void
+bet_free(struct bet *bet);
+
+int
+bet_dump(const struct bet *bet);
+
+#endif
+
