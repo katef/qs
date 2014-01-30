0a6c4010f45f5f7405e31cbf35219ba8d969d742
Kate F <kate@elide.org>
2014-01-30 15:15:31 +0000

    Bugfix; precedence error.

--- src/lex.c
+++ src/lex.c
@@ -72,7 +72,7 @@ lex_push(const char **p, const char **s, const char **e)
 		(*p)++;
 		*s = *p;
 		*p += strcspn(*p, "\'");
-		*e = *(p - 1);
+		*e = *p - 1;
 		(*p) += **p == '\'';
 		return tok_str;
 
