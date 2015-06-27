23475e0da1244f3656a390676a9d47850a44cdf1
Kate F <kate@elide.org>
2015-06-27 14:33:01 +0000

    To background a process.

---
+++ bin/bg.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	pid_t r;
+
+	argv++;
+
+	if (argv[0] == NULL) {
+		fprintf(stderr, "usage: bg cmd [args...]\n");
+		return 1;
+	}
+
+	r = fork();
+	if (r == -1) {
+		perror("fork");
+		return 1;
+	}
+
+	if (r != 0) {
+		return 0;
+	}
+
+	/* TODO: want qs's exec hooks here */
+
+	if (-1 == execvp(argv[0], argv)) {
+		perror(argv[0]);
+		return 1;
+	}
+
+	/* NOTREACHED */
+
+	abort();
+}
+
