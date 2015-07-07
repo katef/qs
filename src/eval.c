9215f81ff14a1049b802a32797464ebb79dfed22
Kate F <kate@elide.org>
2015-07-07 01:28:22 +0000

    A rewrite of the evaulation loop. This uses a cooperative multitasking model, with tasks dispatched on a prioritised round-robin basis.
    The bookkeeping for #pipe is just a placeholder; that will be implemented at a later point.

--- src/eval.c
+++ src/eval.c
@@ -1,12 +1,16 @@
 #define _POSIX_SOURCE
 
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/select.h>
+
+#include <unistd.h>
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
-
-#include <unistd.h>
 
 #include "debug.h"
 #include "var.h"
@@ -19,6 +23,19 @@
 #include "readfd.h"
 #include "status.h"
 #include "builtin.h"
+#include "task.h"
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int self[2]; /* SIGCHLD self pipe */
+
+sigset_t ss_chld; /* SIGCHLD */
+
+struct eval {
+	struct code *code;
+	pid_t pid; /* -1 when not waiting */
+	struct eval *next;
+};
 
 struct pipe_state {
 	int fd[2];
@@ -26,12 +43,31 @@ struct pipe_state {
 	int in, out;
 };
 
+/* TODO: lives in struct task */
 struct tick_state {
-	/* TODO: decide where to store the growing block. need to keep state for re-entry */
+	/* TODO: decide where to store the growing buffer. need to keep state for re-entry */
 	/* XXX: s needs to be kept statefully elsewhere. need state for re-entry */
 	char *s;
 	size_t l;
 };
+
+static void
+sigchld(int s)
+{
+	const char dummy = 'x';
+	int r;
+
+	assert(s == SIGCHLD);
+
+	(void) s;
+
+	do {
+		r = write(self[1], &dummy, sizeof dummy);
+		if (r == -1 && errno != EINTR) {
+			abort();
+		}
+	} while (r != 1);
+}
 
 /* push .s=NULL to data */
 static int
@@ -65,20 +101,30 @@ eval_data(struct data **data, const char *s)
 }
 
 static int
-eval_pipe(struct pipe_state *ps)
+eval_pipe(struct task **next, struct code **code, struct pipe_state *ps)
 {
-	assert(ps != NULL);
 	assert(ps->in != -1);
+	assert(next != NULL);
+	assert(code != NULL);
+	assert(ps != NULL);
 
 	if (-1 == pipe(ps->fd)) {
 		return -1;
 	}
 
+/* TODO: pipe(2) for every fd in the ancestry.
+#dup will close the ones which need closing */
+/* TODO: put these fds somewhere for #run to pick up */
+
+	if (!task_add(next, *code)) {
+		goto error;
+	}
+
+	*code = NULL;
+
 	if (debug & DEBUG_FD) {
 		fprintf(stderr, "pipe [%d,%d]\n", ps->fd[0], ps->fd[1]);
 	}
-
-	/* XXX: close() previous pipe here (this triggers waitpid) */
 
 	ps->usein  = ps->in;
 	ps->in     = ps->fd[0];
@@ -86,6 +132,13 @@ eval_pipe(struct pipe_state *ps)
 	ps->useout = ps->fd[1];
 
 	return 0;
+
+error:
+
+	close(ps->fd[0]);
+	close(ps->fd[1]);
+
+	return -1;
 }
 
 /* pop $?, push !$? to *data */
@@ -142,7 +195,8 @@ eval_call(struct code **next, struct data **data,
 /* eat whole data stack upto .s=NULL, make argv and execute */
 static int
 eval_run(struct code **next, struct data **data,
-	struct frame *frame, struct pipe_state *ps)
+	struct frame *frame, struct pipe_state *ps,
+	struct task *task)
 {
 	struct data *p, *pnext;
 	struct var *v;
@@ -154,6 +208,8 @@ eval_run(struct code **next, struct data **data,
 	assert(data != NULL);
 	assert(frame != NULL);
 	assert(ps != NULL);
+	assert(task != NULL);
+	assert(task->pid == -1);
 
 	if (debug & DEBUG_STACK) {
 		for (p = *data; p->s != NULL; p = p->next) {
@@ -195,7 +251,7 @@ eval_run(struct code **next, struct data **data,
 	}
 
 	/* builtin */
-	switch (builtin(NULL, frame, argc, args)) { /* XXX */
+	switch (builtin(task, frame, argc, args)) {
 	case -1: goto error;
 	case  0: goto done;
 	case  1: break;
@@ -241,13 +297,13 @@ eval_run(struct code **next, struct data **data,
 		break;
 	}
 
-	/* TODO: associate waiting PID with code block for cooperative re-entry */
-
-	return 0;
+	/*
+	 * Cooperative yield to the next task; this task won't be called again until
+	 * our child PID has raised SIGCHLD.
+	 */
+	task->pid = pid;
 
 done:
-
-	/* XXX: why not data_free here? */
 
 	/* TODO: maybe have make_args output p, cut off the arg list, and data_free() it */
 	for (p = *data; p->s != NULL; p = pnext) {
@@ -274,29 +330,91 @@ error:
 /* read from the pipe and push to *data */
 /* note will need to be able to re-enter here after EINTR */
 static int
-eval_tick(struct data **data,
+eval_tick(struct code **next, struct data **data,
 	struct frame *frame, struct pipe_state *ps, struct tick_state *ts)
 {
+	assert(next != NULL);
 	assert(data != NULL);
 	assert(frame != NULL);
 	assert(ps != NULL);
 	assert(ts != NULL);
 
+	/*
+	 * There are two reasons to select here:
+	 *
+	 * Firstly, to avoid blocking on read() when a signal is delivered outside
+	 * the call to read(), and therefore read() will not EINTR.
+	 *
+	 * Secondly, if read() returns for some other reason, so that we don't block
+	 * on reading from the self-pipe when there're no signals to read.
+	 * This could be catered for by only reading from the self pipe when #tick's
+	 * read() gives EINTR, but since we need select() for the first case, I want
+	 * to use the same mechanism for both.
+	 */
+
 	{
+		fd_set fds;
+		int max;
 		int r;
 
-		r = readfd(ps->in, &ts->s, &ts->l);
+		FD_ZERO(&fds);
+		FD_SET(self[0], &fds);
+		FD_SET(ps->in,  &fds);
 
-		if (r == -1 && errno == EINTR) {
-			return 0;
-		}
+		max = MAX(self[0], ps->in);
 
+		r = select(max + 1, &fds, NULL, NULL, NULL);
 		if (r == -1) {
 			return -1;
 		}
 
-		if (debug & DEBUG_VAR) {
-			fprintf(stderr, "` read: %.*s\n", (int) ts->l, ts->s);
+		/*
+		 * It doesn't matter which fd we deal with first, since the parent's
+		 * self[1] will keep the pipe open, so we can't accientally read EOF
+		 * before yielding to spawn the next child.
+		 */
+
+		if (FD_ISSET(self[0], &fds)) {
+			char dummy;
+
+			do {
+				r = read(self[0], &dummy, sizeof dummy);
+			} while (r == 0);
+			if (r == -1) {
+				return -1;
+			}
+
+			assert(dummy == 'x');
+
+			/* If ps->in is also ready, we'll deal with that on re-entry. */
+
+			goto yield;
+		}
+
+		if (FD_ISSET(ps->in, &fds)) {
+			if (-1 == sigprocmask(SIG_UNBLOCK, &ss_chld, NULL)) {
+				perror("sigprocmask SIG_UNBLOCK");
+				goto fail;
+			}
+
+			r = readfd(ps->in, &ts->s, &ts->l);
+
+			if (-1 == sigprocmask(SIG_BLOCK, &ss_chld, NULL)) {
+				perror("sigprocmask SIG_BLOCK");
+				goto fail;
+			}
+
+			if (r == -1 && errno == EINTR) {
+				goto yield;
+			}
+
+			if (r == -1) {
+				return -1;
+			}
+
+			if (debug & DEBUG_VAR) {
+				fprintf(stderr, "` read: %.*s\n", (int) ts->l, ts->s);
+			}
 		}
 	}
 
@@ -317,6 +435,27 @@ ts->s = NULL; /* XXX: doesn't belong here */
 
 	return 0;
 
+yield:
+
+	/*
+	 * This node was popped, but reading was interrupted by SIGCHILD;
+	 * here we yield to run that child's task, but first push a replacement
+	 * #tick for re-entry to continue reading where we left off.
+	 * This means the evalulation loop does not need to have special handling
+	 * for #tick being re-entrant.
+	 *
+	 * However the evaulation loop does need to know that read() was
+	 * interrupted by SIGCHILD (and only SIGCHLD) here, so that it can
+	 * wait() to reap the exited child. So we return -1 with EINTR here.
+	 */
+
+	if (!code_push(next, frame, CODE_TICK)) {
+		goto error;
+	}
+
+	errno = EINTR;
+	return -1;
+
 error:
 
 	free(ts->s);
@@ -324,15 +463,20 @@ error:
 ts->s = NULL; /* XXX: doesn't belong here */
 
 	return -1;
+
+fail:
+
+	abort();
 }
 
 /* TODO: explain what happens here: status.r is the predicate, u.code is a block to call */
 static int
 eval_if(struct code **next, struct data **data,
-	struct code *code)
+	struct code **code)
 {
 	assert(next != NULL);
 	assert(data != NULL);
+	assert(code != NULL);
 
 	/*
 	 * When the status condition is true, we simply ignore the u.code pointer.
@@ -347,11 +491,15 @@ eval_if(struct code **next, struct data **data,
 			code_dump(stderr, *next);
 		}
 
-		if (!code_wind(next, code)) {
+		/* TODO: would prefer to transplant the code as-is,
+		 * rather than copying and then code_free */
+		if (!code_wind(next, *code)) {
 			return -1;
 		}
 
-		*next = code;
+		code_free(*code);
+
+		*code = NULL;
 	}
 
 	return 0;
@@ -360,12 +508,13 @@ eval_if(struct code **next, struct data **data,
 /* TODO: explain what happens here */
 static int
 eval_set(struct data **data,
-	struct frame *frame, struct code *code)
+	struct frame *frame, struct code **code)
 {
 	struct data *a;
 
 	assert(data != NULL);
 	assert(frame != NULL);
+	assert(code != NULL);
 
 	if (*data == NULL) {
 		errno = 0;
@@ -374,10 +523,12 @@ eval_set(struct data **data,
 
 	a = data_pop(data);
 
-	if (!frame_set(frame, strlen(a->s), a->s, code)) {
+	if (!frame_set(frame, strlen(a->s), a->s, *code)) {
 		errno = EINVAL;
 		return -1;
 	}
+
+	*code = NULL;
 
 	free(a);
 
@@ -432,64 +583,202 @@ eval_binop(struct data **data,
 	return 0;
 }
 
-int
-eval(struct code *code, struct data **data)
+static int
+eval_task(struct task *task, struct data **data,
+	struct pipe_state *ps, struct tick_state *ts)
 {
-	struct code *node, *next;
+	struct code *node;
+	int r;
+
+	assert(data != NULL);
+	assert(task != NULL);
+
+	/*
+	 * There's no need for this loop; we could just take a single instruction
+	 * at a time, and round-robin across all tasks. But while we're here,
+	 * it's no trouble to evaulate as much as we can before needing to sleep.
+	 *
+	 * The code list may be NULL if this task is waiting for a child, but has
+	 * nothing to execute after that.
+TODO: in which case, would it be okay to remove the task and consider the child a stray?
+	 */
+
+	while (task->code != NULL) {
+		node = code_pop(&task->code);
+
+		assert(node != NULL);
+
+		if (debug & DEBUG_EVAL) {
+			fprintf(stderr, "code: "); code_dump(stderr,  node);
+			fprintf(stderr, "data: "); data_dump(stderr, *data);
+		}
+
+		switch (node->type) {
+		case CODE_NULL: r = eval_null(data);                                     break;
+		case CODE_DATA: r = eval_data(data, node->u.s);                          break;
+		case CODE_PIPE: r = eval_pipe(&task->next, &node->u.code, ps);           break;
+		case CODE_NOT:  r = eval_not ();                                         break;
+		case CODE_IF:   r = eval_if  (&task->code, data, &node->u.code);         break;
+		case CODE_RUN:  r = eval_run (&task->code, data, node->frame, ps, task); break;
+		case CODE_CALL: r = eval_call(&task->code, data, node->frame);           break;
+		case CODE_TICK: r = eval_tick(&task->code, data, node->frame, ps, ts);   break;
+		case CODE_SET:  r = eval_set (data, node->frame, &node->u.code);         break;
+		case CODE_JOIN: r = eval_binop(data, node->frame, op_join);              break;
+
+		default:
+			code_free(node);
+			errno = EINVAL;
+			return -1;
+		}
+
+		code_free(node);
+
+		if (r == -1) {
+			return -1;
+		}
+
+		if (task->pid != -1) {
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
+static int
+eval_main(struct code *code, struct data **data)
+{
+	struct task *tasks, *t;
 	struct pipe_state ps;
 	struct tick_state ts;
 	int r;
 
 	assert(data != NULL);
 
-	next = code;
+	tasks = NULL;
+
+	if (!task_add(&tasks, code)) {
+		goto error;
+	}
 
 	/* XXX: this should be the only place where STDIN_FILENO and STDOUT_FILENO are set */
 	/* XXX: setting all of these is not neccessary. some should be -1 */
 	ps.in  = ps.usein  = STDIN_FILENO;
 	ps.out = ps.useout = STDOUT_FILENO;
 
-	/* XXX: will need an independent ts per context. ditto for pipe state */
+	/* XXX: will need an independent ts per task. ditto for pipe state */
 	ts.s = NULL;
 
-	while (node = next, node != NULL) {
-		if (debug & DEBUG_EVAL) {
-			fprintf(stderr, "code: "); code_dump(stderr,  node);
-			fprintf(stderr, "data: "); data_dump(stderr, *data);
+	/*
+	 * Terminology: What blocking (and sleeping) mean
+	 *
+	 * A signal may be blocked by SIG_BLOCK, or by sigaction.sa_mask.
+	 * When blocked, subsequent arrivals of the same signal number will be
+	 * consolidated (effectively a queue of one element per signal; a bitmap).
+	 * One instance will then be delivered when unblocked.
+	 *
+	 * A syscall will not return for an indefinite period.
+	 * For waitpid() without the WNOHANG flag. I call this a blocking wait.
+	 *
+	 * A task has a child process, and its code cannot continue
+	 * evaulation until that child has been waited on. I call this sleeping.
+	 * A sleeping task is indicated by task.pid set to the child's PID.
+	 */
+
+	/*
+	 * There are two reasons a task will cooperatively yield;
+	 * either because #tick's read(2) was interrupted by SIGCHLD
+	 * or because #run was evaulated, and now the task is sleeping.
+	 *
+	 * #tick may be re-entered to continue reading where it left off.
+	 * However, a task waiting for a child will not run again
+	 * until its child raises SIGCHLD.
+	 *
+	 * When a child is reaped (either by a blocking wait, or after SIGCHLD
+	 * causing EINTR for some other running task), the associated child's
+	 * task is prioritised to the head of the tasks list.
+	 *
+	 * For #tick interrupted when reading from its pipe, this ensures that
+	 * the next tasks re-entered are to (perhaps) spawn more children
+	 * to write, rather than re-entering the #tick immediately which would
+	 * block forever on read(2), with no child at the write end.
+	 */
+
+	t = tasks;
+
+	for (;;) {
+		r = eval_task(t, data, &ps, &ts);
+
+		if (r == -1 && errno != EINTR) {
+			goto error;
 		}
 
-		/* TODO: code_pop, and free code item */
+		if (r == -1 && errno == EINTR) {
+			int n;
 
-		next = node->next;
+			/*
+			 * One situation to note here: SIGCHILD is blocked (per SIG_BLOCK)
+			 * while waiting (and subsequently waiting again with WNOHANG
+			 * until all children are reaped), SIGCHLD will be delivered on
+			 * the next SIG_UNBLOCK. On handling that, we'd find there is no
+			 * child to reap, having reaped them already.
+			 *
+			 * So this has to be a WNOHANGing wait otherwise we'd block here
+			 * forever. Alternately sigsuspend(2) could be used, but I don't
+			 * want to depend on POSIX.2001 additions. So instead I'm happy
+			 * to ignore "stray child" signals for this situation.
+			 */
+			n = task_wait(&tasks, -1, WNOHANG);
+			if (n == -1) {
+				goto error;
+			}
 
-		switch (node->type) {
-		case CODE_NULL: r = eval_null(data);                               break;
-		case CODE_DATA: r = eval_data(data, node->u.s);                    break;
-		case CODE_PIPE: r = eval_pipe(&ps);                                break;
-		case CODE_NOT:  r = eval_not ();                                   break;
-		case CODE_IF:   r = eval_if  (&next, data, node->u.code);          break;
-		case CODE_RUN:  r = eval_run (&next, data, node->frame, &ps);      break;
-		case CODE_CALL: r = eval_call(&next, data, node->frame);           break;
-		case CODE_TICK: r = eval_tick(data, node->frame, &ps, &ts);        break;
-		case CODE_SET:  r = eval_set (data, node->frame, node->u.code);    break;
-		case CODE_JOIN: r = eval_binop(data, node->frame, op_join);        break;
-
-		default:
-			errno = EINVAL;
-			return -1;
+			/*
+			 * task_wait() returns the number of newly prioritised tasks.
+			 * If this is zero, then there are no more children to #run;
+			 * We can close our copy of the write end of the #tick pipe,
+			 * since we don't need to hold open the pipe any more.
+			 * Subsequent read() in #tick will now give EOF.
+			 */
+			if (n == 0) {
+				close(ps.out);
+			}
 		}
 
-		if (r == -1) {
-			return -1;
+		if (t->code == NULL && t->pid == -1) {
+			task_remove(&tasks, t);
 		}
 
-		if (-1 == proc_wait(-1, 0)) {
-			perror("wait");
-			continue;
+		if (tasks == NULL) {
+			break;
 		}
 
-/* TODO: cooperative re-entry to the appropriate code stack. need PID->code lookup */
+		t = task_next(tasks);
+
+		/*
+		 * When task_next() returns NULL, all tasks are sleeping on children.
+		 * They are waited for using a blocking wait here, because there's
+		 * nothing else to do.
+		 */
+		if (t == NULL) {
+			if (-1 == task_wait(&tasks, -1, 0)) {
+				goto error;
+			}
+
+			t = tasks;
+		}
+
+		/*
+		 * At this point the task list is ordered to re-enter tasks which were
+		 * waiting for SIGCHLD in preference to tasks which were not waiting.
+		 */
 	}
+
+	/*
+	 * At this point there are no remaining tasks,
+	 * and therefore no children to wait for.
+	 */
+	assert(tasks == NULL);
 
 	if (debug & DEBUG_EVAL) {
 		fprintf(stderr, "eval out: ");
@@ -497,5 +786,74 @@ eval(struct code *code, struct data **data)
 	}
 
 	return 0;
+
+error:
+
+	/* TODO: free tasks */
+	/* TODO: kill children? probably not. wait(2) for all? WNOHANG */
+
+	return -1;
+}
+
+int
+eval(struct code *code, struct data **data)
+{
+	struct sigaction sa, sa_old;
+	sigset_t ss_old;
+	int r;
+
+	if (-1 == pipe(self)) {
+		perror ("pipe");
+		return -1;
+	}
+
+	if (-1 == sigemptyset(&ss_chld)) {
+		perror("sigemptyset");
+		return -1;
+	}
+
+	if (-1 == sigaddset(&ss_chld, SIGCHLD)) {
+		perror("sigaddset");
+		return -1;
+	}
+
+	if (-1 == sigprocmask(SIG_BLOCK, &ss_chld, &ss_old)) {
+		perror("sigprocmask SIG_UNBLOCK");
+		goto fail;
+	}
+
+	sa.sa_handler = sigchld;
+	sa.sa_flags   = SA_NOCLDSTOP;
+
+	if (sigaction(SIGCHLD, &sa, &sa_old)) {
+		perror("sigaction");
+		goto fail;
+	}
+
+	r = eval_main(code, data);
+
+	if (sigaction(SIGCHLD, &sa_old, NULL)) {
+		perror ("sigaction");
+		goto fail;
+	}
+
+	if (-1 == sigprocmask(SIG_SETMASK, &ss_old, NULL)) {
+		perror("sigprocmask SIG_SETMASK");
+		goto fail;
+	}
+
+	close(self[0]);
+	close(self[1]);
+
+	return r;
+
+fail:
+
+	/*
+	 * Failures which would leave the process in an altered state;
+	 * I don't see how we could expect to recover here.
+	 */
+
+	abort();
 }
 
