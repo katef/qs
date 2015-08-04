63f4a14a1d0df6bf8dc3334e305d816f890f4c6a
Kate F <kate@elide.org>
2015-08-04 12:30:24 +0000

    Remove the loop around `read(2)`; this had the effect of containing signals interrupting after data is read, which need to be exposed so that the evalulator may then re-`select(2)` on its self-pipe.

--- src/readfd.c
+++ src/readfd.c
@@ -29,34 +29,34 @@ readfd(int fd, char **s, size_t *n)
 		l = *n;
 	}
 
-	do {
-		if (*n % PIPE_BUF == 0) {
-			char *tmp;
+	if (*n % PIPE_BUF == 0) {
+		char *tmp;
 
-			if (l + PIPE_BUF <= l) {
-				errno = ENOMEM;
-				goto error;
-			}
-
-			l += PIPE_BUF;
-
-			tmp = realloc(*s, l);
-			if (tmp == NULL) {
-				goto error;
-			}
-
-			*s = tmp;
-		}
-
-		r = read(fd, *s + *n, l - *n);
-		if (r == -1) {
+		if (l + PIPE_BUF <= l) {
+			errno = ENOMEM;
 			goto error;
 		}
 
-		*n += r;
-	} while (r != 0);
+		l += PIPE_BUF;
 
-	{
+		tmp = realloc(*s, l);
+		if (tmp == NULL) {
+			goto error;
+		}
+
+		*s = tmp;
+	}
+
+	r = read(fd, *s + *n, l - *n);
+	if (r == -1 && errno == EINTR) {
+		return -1;
+	}
+
+	if (r == -1) {
+		goto error;
+	}
+
+	if (r == 0) {
 		char *tmp;
 
 		tmp = realloc(*s, *n + 1);
@@ -67,15 +67,19 @@ readfd(int fd, char **s, size_t *n)
 		*s = tmp;
 
 		*n += 1;
+
+		(*s)[*n] = '\0';
+
+		return 0;
 	}
 
-	(*s)[*n] = '\0';
+	*n += r;
 
-	return 0;
+	return (int) r;
 
 error:
 
-	if (errno != EINTR) {
+	{
 		int e;
 
 		e = errno;
