7e9f57228e5c5d68cb0b6e45482a2d30f92d1f3f
Kate F <kate@elide.org>
2015-08-10 14:54:17 +0000

    Add `+/-` to unset as well as set flags for `-d`.

--- src/main.c
+++ src/main.c
@@ -31,27 +31,42 @@ static struct frame *top;
 static int
 debug_flags(const char *s)
 {
+	int v;
+
 	assert(s != NULL);
 
+	v = 1;
+
 	for ( ; *s != '\0'; s++) {
+		int e;
+
 		switch (*s) {
-		case 'a': debug = ~0U;          break;
-		case 'b': debug |= DEBUG_BUF;   break;
-		case 'l': debug |= DEBUG_LEX;   break;
-		case 'p': debug |= DEBUG_PARSE; break;
-		case 'c': debug |= DEBUG_ACT;   break;
-		case 'f': debug |= DEBUG_FRAME; break;
-		case 's': debug |= DEBUG_STACK; break;
-		case 'v': debug |= DEBUG_VAR;   break;
-		case 'e': debug |= DEBUG_EVAL;  break;
-		case 'x': debug |= DEBUG_EXEC;  break;
-		case 'r': debug |= DEBUG_PROC;  break;
-		case 'g': debug |= DEBUG_SIG;   break;
-		case 'd': debug |= DEBUG_FD;    break;
+		case '+': v = 1; continue;
+		case '-': v = 0; continue;
+
+		case 'a': e = ~0U;         break;
+		case 'b': e = DEBUG_BUF;   break;
+		case 'l': e = DEBUG_LEX;   break;
+		case 'p': e = DEBUG_PARSE; break;
+		case 'c': e = DEBUG_ACT;   break;
+		case 'f': e = DEBUG_FRAME; break;
+		case 's': e = DEBUG_STACK; break;
+		case 'v': e = DEBUG_VAR;   break;
+		case 'e': e = DEBUG_EVAL;  break;
+		case 'x': e = DEBUG_EXEC;  break;
+		case 'r': e = DEBUG_PROC;  break;
+		case 'g': e = DEBUG_SIG;   break;
+		case 'd': e = DEBUG_FD;    break;
 
 		default:
 			fprintf(stderr, "-d: unrecognised flag '%c'\n", *s);
 			return -1;
+		}
+
+		if (v) {
+			debug |=  e;
+		} else {
+			debug &= ~e;
 		}
 	}
 
@@ -215,7 +230,7 @@ error:
 
 usage:
 
-	fprintf(stderr, "usage: qs [-d ablpcfsvexrd]\n");
+	fprintf(stderr, "usage: qs [-d +-ablpcfsvexrd]\n");
 
 	return 1;
 }
